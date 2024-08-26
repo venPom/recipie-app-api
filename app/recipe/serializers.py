324c213dd30ea8a4c0722c6fce31b88700be6da8
Jameel <jameel@hellotractor.com>
2024-08-26 11:53:31 +0300

    Added upload image feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -44,3 +44,11 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view."""
     ingredients = IngredientSerializer(many=True, read_only=True)
     tags= TagSerializer(many=True, read_only=True)
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for tags."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only_fields = ['id']
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,11 +5,14 @@ Views for the recipe APIs
 from rest_framework import (
     viewsets,
     mixins,
+    status,
 )
-
-from recipe import serializers
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+
+from recipe import serializers
 
 from core.models import (
     Tag,
@@ -56,8 +59,22 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Return the serializer class for request."""
         if self.action == 'retrieve':
             return serializers.RecipeDetailSerializer
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerializer
 
         return self.serializer_class
     
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload an image to recipe."""
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
